a56872b8b3b0859364528854c2de2027517ded89
ahmutty <125568753+ahmutty@users.noreply.github.com>
2023-07-22 21:10:35 +0800

    Update and rename relitu.py to thermodynamic chart.py

--- xunlian/thermodynamic chart.py
+++ xunlian/thermodynamic chart.py
@@ -1,8 +1,6 @@
 
-##  基础函数库
 import numpy as np
 import pandas as pd
-## 绘图函数库
 import matplotlib.pyplot as plt
 import seaborn as sns
 data = pd.read_csv('data1.csv')
@@ -15,6 +13,6 @@ plt.rcParams['font.sans-serif'] = ['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
 sns.heatmap(data.corr(), cmap="YlGnBu", annot=True)
 plt.xticks(rotation = 45,fontsize=12)
-plt.yticks(fontsize=12)  # 设置坐标标签字体大小
+plt.yticks(fontsize=12)  
 plt.savefig( 'reli.png')
 plt.show()
